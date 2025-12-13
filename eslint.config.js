ba3996ecf0851b8af2001a07e5721aff8bcf7e63
Botardo Dev <dev@botardo.app>
2025-12-13 21:46:55 +0100

    chore: exclude backup folders from eslint baseline (-56% errors)

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,12 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores([
+    'dist',
+    'src_backup*/**',
+    'app_source_export/**',
+    'backup/**',
+  ]),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
